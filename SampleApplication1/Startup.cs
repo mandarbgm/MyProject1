dc3c55e898f5a2173cf671ab44622cd3bce294d5
Mandar Kulkarni <mandarpune@outlook.com>
2018-08-18 16:21:21 +0530

    First Commit
    Signed-off-by: Mandar Kulkarni <mandarpune@outlook.com>

---
+++ SampleApplication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleApplication1.Startup))]
+namespace SampleApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
